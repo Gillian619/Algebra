ca0bb9983c7ed74a394e193d4e5d3dc0a85c6397
IsisTheOne <alenacard619@gmail.com>
2023-05-22 15:46:57 -0300

    22/05/2023

--- js/script.js
+++ js/script.js
@@ -6,11 +6,11 @@ function circulo() {
 function hip() {
     let co = prompt("insira o cateto oposto")
     let ca = prompt("insira o cateto adjascente")
-    alert("o valor da hipotenusa é =" + Math.sqrt((co * co) + (ca * ca)))
+    alert("o valor da hipotenusa é =" + Math.sqrt(Math.pow(co, 2) + Math.pow(ca, 2)))
 }
 function dolar() {
-    const dol = 4.93
-    let real = prompt("insira o valor em reais(R$)")
+    let dol = parseFloat(prompt("insira a cotação atual do dolar($)"))
+    let real = parseFloat(prompt("insira o valor em reais(R$)"))
     alert("o valor em dolar é =" + real * dol)
 }
 function degree() {
@@ -18,17 +18,17 @@ function degree() {
     alert("A temperatura em fahrenheit é =" + (ce * 1.8) + 32)
 }
 function media() {
-    let n1 = parseInt(prompt("identifique a primeira nota"))
-    let n2 = parseInt(prompt("identifique a segunda nota"))
-    let n3 = parseInt(prompt("identifique a terceira nota"))
+    let n1 = parseFloat(prompt("identifique a primeira nota"))
+    let n2 = parseFloat(prompt("identifique a segunda nota"))
+    let n3 = parseFloat(prompt("identifique a terceira nota"))
     alert("a sua nota media é " + (n1 + n2 + n3) / 3)
 }
 function mascara() {
-    let a = parseInt(prompt("identifique a a"))
-    let b = parseInt(prompt("identifique a b"))
-    let c = parseInt(prompt("identifique a c"))
-    let delta = parseInt((b * b) - 4 * (a * c))
-    let x1 = parseInt(-b + (delta * delta) / (2 * a))
-    let x2 = parseInt(-b - (delta * delta) / (2 * a))
-    alert("o valor de x1 e x2 são =" + x1 + "e" + x2)
+    let a = parseFloat(prompt("identifique a a"))
+    let b = parseFloat(prompt("identifique a b"))
+    let c = parseFloat(prompt("identifique a c"))
+    let delta = parseFloat(Math.pow(b, 2) - 4 * a * c)
+    let x1 = parseFloat((-b + Math.sqrt(delta)) / (2 * a))
+    let x2 = parseFloat((-b - Math.sqrt(delta)) / (2 * a))
+    alert("o valor de x1 e x2 são =" + x1 + " e " + x2)
 }
